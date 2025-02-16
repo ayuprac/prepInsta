ee84cabfa2387fb921762b7f07278dd4e48c8571
Ayush k <ayushkhorgade2002@gmail.com>
2025-02-16 23:19:33 +0530

    even/odd using ternary operator

--- one_two.java
+++ one_two.java
@@ -13,6 +13,7 @@ public class one_two{
         System.out.println("no is zero");
        }
     }*/
+    /*
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter a number: ");
@@ -20,5 +21,11 @@ public class one_two{
         if(number % 2 == 0){
             System.out.println(number + " ? ,its a even number");
         }else{System.out.println("its a odd number");}
+    }*/
+    public static void main(String[] args) {
+        //using ternary operator
+        int num = 20;
+        String status = num%2==0 ? " is even" : " is odd";
+        System.out.println(num + status);
     }
 }
