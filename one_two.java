139a6c19713c4b8b6a6d6e672394cbb23b10835e
Ayush k <ayushkhorgade2002@gmail.com>
2025-02-16 23:07:15 +0530

    even/odd

--- one_two.java
+++ one_two.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 public class one_two{
+   /* 
     public static void main(String[] args) {
        System.out.println("enter a no.: "); 
        Scanner sc = new Scanner(System.in);
@@ -11,5 +12,13 @@ public class one_two{
        }else{
         System.out.println("no is zero");
        }
+    }*/
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number: ");
+        int number = sc.nextInt();
+        if(number % 2 == 0){
+            System.out.println(number + " ? ,its a even number");
+        }else{System.out.println("its a odd number");}
     }
 }
