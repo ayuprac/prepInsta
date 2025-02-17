a2163ed8d44c9778b4271f9b3ecf41669226e86a
Ayush k <ayushkhorgade2002@gmail.com>
2025-02-17 15:01:30 +0530

    sum of n numbers

---
+++ three.java
@@ -0,0 +1,10 @@
+import java.util.Scanner ;
+public class three {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int num = sc.nextInt();
+        int sum = (num*(num+1))/2;
+        System.out.println("the sum of "+num+" natural numbers is : "+sum);
+    }
+}
