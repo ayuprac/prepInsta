655f19a98f63ddd27ee1f1daed7f1624ded3b388
Ayush k <ayushkhorgade2002@gmail.com>
2025-02-17 15:07:20 +0530

    using for loop

--- three.java
+++ three.java
@@ -4,7 +4,15 @@ public class three {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter a number");
         int num = sc.nextInt();
-        int sum = (num*(num+1))/2;
-        System.out.println("the sum of "+num+" natural numbers is : "+sum);
+       /*  int sum = (num*(num+1))/2;
+        System.out.println("the sum of "+num+" natural numbers is : "+sum);*/
+        //using for loop
+        int sum = 0;
+
+        for(int i=1;i<=num;i++){
+         sum = sum+i;
+        }
+        System.out.println("the sum of "+num +" natural numbers is "+sum);  
+
     }
 }
