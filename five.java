36b29b799ad657c39c8260544497fbe343ad476f
Ayush k <ayushkhorgade2002@gmail.com>
2025-02-18 15:44:01 +0530

    sum in range using recursion

--- five.java
+++ five.java
@@ -2,20 +2,30 @@
 public class five {
     public static void main(String[] args) {
         int a = 5,b=10;
-        int sum = 0;
-        for (int i = a; i <= b; i++)
-        sum = sum + i;
-      System.out.println ("The sum is " + sum);
+        
         
         //using for loop
-        /*for(int i = a;i<b;i++){
+        
+        /*
+        int sum = 0;
+        for(int i = a;i<=b;i++){
             sum = sum +i;
         }
         System.out.println("the sum of numbers in range: "+sum);*/
         //using formula
-        int sums = (b*(b+1)/2) - (a*(a+1)/2) + a;
-        System.out.println("sum is :"+sums);
-        
+       /*  int sums = (b*(b+1)/2) - (a*(a+1)/2) + a;
+        System.out.println("sum is :"+sums);*/
+       //using recursion
 
+        int sum = getSum(0,a,b);
+        System.out.println("the sum is :"+sum);
+
+    }
+    static int getSum(int sum,int i,int b){
+        //base case
+        if(i>b)      //stop when call go over b
+        return sum;
+
+        return i +getSum(sum,i+1,b);
     }
 }
