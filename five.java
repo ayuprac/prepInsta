2447291c8cf5ea85f5d9f13ebc36b89811000833
Ayush k <ayushkhorgade2002@gmail.com>
2025-02-18 15:24:00 +0530

    sum of nums in a range using for loop

---
+++ five.java
@@ -0,0 +1,12 @@
+// sum of numbers in a given range
+public class five {
+    public static void main(String[] args) {
+        int a = 5,b=10;
+        int sum = 0;
+        for(int i = a;i<b;i++){
+            sum = sum +i;
+        }
+        System.out.println("the sum of numbers in range: "+sum);
+
+    }
+}
