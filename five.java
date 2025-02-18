207ef7ce33e1b53b02b006ade41f5ac974b932d5
Ayush k <ayushkhorgade2002@gmail.com>
2025-02-18 15:36:34 +0530

    range sum using formula

--- five.java
+++ five.java
@@ -3,10 +3,19 @@ public class five {
     public static void main(String[] args) {
         int a = 5,b=10;
         int sum = 0;
-        for(int i = a;i<b;i++){
+        for (int i = a; i <= b; i++)
+        sum = sum + i;
+      System.out.println ("The sum is " + sum);
+        
+        //using for loop
+        /*for(int i = a;i<b;i++){
             sum = sum +i;
         }
-        System.out.println("the sum of numbers in range: "+sum);
+        System.out.println("the sum of numbers in range: "+sum);*/
+        //using formula
+        int sums = (b*(b+1)/2) - (a*(a+1)/2) + a;
+        System.out.println("sum is :"+sums);
+        
 
     }
 }
