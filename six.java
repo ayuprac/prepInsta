31a0ffeffe7f515f809214321a67ddeed66dd0d8
Ayush k <ayushkhorgade2002@gmail.com>
2025-02-19 14:27:58 +0530

    ternary operator

--- six.java
+++ six.java
@@ -2,6 +2,7 @@ public class six{
     public static void main(String[] args) {
         int a = 60;
         int b = 65;
+        /* 
         if(a>b){
             System.out.println(a+ " is greater than "+b);
         }else if (a<b){
@@ -9,6 +10,13 @@ public class six{
 
         }else{
             System.out.println("both are equal");
+        }*/
+        
+        if(a==b){System.out.println("both are equal");}
+        else{
+            int temp = a > b ? a : b ;
+            System.out.println(temp+ "is largest");
         }
+
     }
 }
