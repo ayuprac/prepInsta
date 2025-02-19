816b80dc6cc0d3cb3dc672ef901c98504dda09c6
Ayush k <ayushkhorgade2002@gmail.com>
2025-02-19 14:23:16 +0530

    greater no using if

---
+++ six.java
@@ -0,0 +1,14 @@
+public class six{
+    public static void main(String[] args) {
+        int a = 60;
+        int b = 65;
+        if(a>b){
+            System.out.println(a+ " is greater than "+b);
+        }else if (a<b){
+            System.out.println(b+ " is greater than "+a);
+
+        }else{
+            System.out.println("both are equal");
+        }
+    }
+}
