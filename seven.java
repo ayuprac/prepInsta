71ddd7da63185f9c03c8aa45e1eb62d5fa4b3399
Ayush k <ayushkhorgade2002@gmail.com>
2025-02-20 10:59:53 +0530

    using ternary operator

--- seven.java
+++ seven.java
@@ -1,13 +1,19 @@
 // Using && operator
 public class seven{
     public static void main(String[] args) {
-        int a = 400,b=60,c=8;
+        int a = 400,b=60,c=8000;
+        /* 
         if(a>b && a>c){
         System.out.println(a+ " is greater");
         }else if(b>a && b>c){
             System.out.println(b+" is greater");
         }else{
             System.out.println(c +" is greater");
-        }
+        }*/
+        //using ternary operator
+        int temp,result;
+        temp = a > b ? a:b;
+        result = temp>c ?temp:c;
+        System.out.println(result + " is greater ");
     }
 }
