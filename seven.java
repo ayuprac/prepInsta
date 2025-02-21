2a51ec04cf8e05414f18e4c5031e49b7df4e1a38
Ayush k <ayushkhorgade2002@gmail.com>
2025-02-21 11:21:12 +0530

    issue resolved

--- seven.java
+++ seven.java
@@ -2,10 +2,6 @@
 public class seven{
     public static void main(String[] args) {
         int a = 40,b=60,c=800;
-<<<<<<< HEAD
-        //using if else
-=======
->>>>>>> 54c020258302da5bfe095107db81c1b40ae64a01
         /* 
         if(a>b && a>c){
         System.out.println(a+ " is greater");
