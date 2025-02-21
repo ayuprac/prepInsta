0aa82610ba186415b48e58f80db0242c5eeb4e89
Ayush k <ayushkhorgade2002@gmail.com>
2025-02-21 11:20:41 +0530

    leap using if

--- eight.java
+++ eight.java
@@ -1,12 +1,19 @@
 public class eight {
     public static void main(String[] args) {
         int year = 2000;
+        /*
         if(year % 400 ==0 ){
             System.out.println(year+ " is a leap year");
         }else if(year %4==0 && year % 100!=0){
            System.out.println(year+  "is a leap year");
         }else{
             System.out.println(year+ "is not a leap year");
+        }*/
+        //second way
+        if(year%400==0 || (year%4==0&&year%100!=0)){
+            System.out.println(year+ " is leap.");
+        }else{
+            System.out.println("not a leap year");
         }
     }
 }
