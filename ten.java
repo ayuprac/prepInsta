e89ad6dff8d907fa234063a232185367bf7efcc0
Ayush k <ayushkhorgade2002@gmail.com>
2025-02-22 10:34:28 +0530

    inefficent code using n/2

--- ten.java
+++ ten.java
@@ -5,6 +5,11 @@ public class ten {
             if(isPrime(i))
             System.out.println(i);
         }
+        //other way
+        for(int i = lower;i<=upper;i++){
+            if(isprime2(i))
+            System.out.print(i +" ");
+        }
        
     }
     static boolean isPrime(int n){
@@ -15,5 +20,17 @@ public class ten {
             return false;
         }
         return true;
+    }
+    static boolean isprime2(int n){
+      int count = 0;
+    if(n<2)
+    return false;
+    for(int i =2;i<=n/2;i++){
+        if(n % i ==0){
+            return false;
+        }
+    }
+    return true;
+
     }
 }
