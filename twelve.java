594aac4a065a8b7085cd136e524f79abcd2e3255
Ayush k <ayushkhorgade2002@gmail.com>
2025-02-24 15:59:34 +0530

    reversed using recursion

--- twelve.java
+++ twelve.java
@@ -1,4 +1,5 @@
 public class twelve{
+    /*
     public static void main(String[] args) {
         
         int num = 24589,reverse = 0,rem;
@@ -10,5 +11,18 @@ public class twelve{
         }
        
         System.out.println("reversed no: "+reverse);
+    } */
+
+    public static void main(String[] args) {
+        int num = 245789,rev=0;
+        System.out.println("Reversed no: "+getReverse(num,rev));
+    }
+    static int getReverse(int num,int rev){
+        if(num == 0)
+        return rev;
+        int rem = num%10;
+        rev = rev*10 +rem;
+
+        return getReverse(num/10,rev);
     }
 }
