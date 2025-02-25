a313cf572b59b893c1389e432ac1b0b84ffe9981
Ayush k <ayushkhorgade2002@gmail.com>
2025-02-25 15:18:22 +0530

    palindrome using recursion

--- thirteen.java
+++ thirteen.java
@@ -1,6 +1,7 @@
 public class thirteen {
     public static void main(String[] args) {
         int num = 1256521,reverse = 0,rem;
+        /* 
         int temp = num;
         while(temp!=0){
             rem = temp %10;
@@ -10,6 +11,19 @@ public class thirteen {
         if(reverse == num)
         System.out.println("number "+num+" is palindrome");
         else System.out.println("number is not palindrome");
+        */
+
+        if(getReverse(num,reverse)==num)
+        System.out.println(num+ " is Palindrome");
+        else System.out.println(num+ " is not palindrome");
+    }
+    static int getReverse(int num,int rev){
+        if(num==0)
+         return rev;
+
+        int rem = num % 10;
+        rev = rev * 10 + rem;
+         return getReverse(num/10,rev);
     }
     
 }
