05d79c1b0c7456db2bbf544ad316a1263d4062e6
Ayush k <ayushkhorgade2002@gmail.com>
2025-03-06 23:15:15 +0530

    fibonacci series

--- eighteen.java
+++ eighteen.java
@@ -2,14 +2,14 @@ public class eighteen {
     public static void main(String[] args) {
         int first = 0;
         int second = 1;
-        int sum;
-        int n = 6;
+        int nextterm;
+        int n = 7;
         System.out.print(first + " "+second);
         for(int i = 3;i<=n;i++){
-            sum = first + second;
-            System.out.print(" "+sum);
+            nextterm = first + second;
+            System.out.print(" "+nextterm);
             first = second;
-            second = sum;
+            second = nextterm;
 
         }
     }
